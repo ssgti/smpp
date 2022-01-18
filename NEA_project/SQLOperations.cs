36895da4e780d69fdb10bb0f561679298b12a7e4
will <williampaulgeorge@outlook.com>
2022-01-18 17:48:41 +0000

    add comments

--- NEA_project/SQLOperations.cs
+++ NEA_project/SQLOperations.cs
@@ -10,11 +10,16 @@ using MySql.Data.MySqlClient;
 
 namespace NEA_project
 {
-    static class SQLOperations
+    static class SQLOperations 
     {
-        private const string connStr = "server=127.0.0.1;user=snoop;database=mydb;port=3306;password=snoopin321";
+        private const string connStr = "server=127.0.0.1;user=snoop;database=mydb;port=3306;password=snoopin321"; // only change this here
 
-        public static List<string> sqlSelect(string query)
+           // keeping all SQL code in one place to save me having to retype it all every time it's needed
+          //  these functions are called from other parts of the program passing the queries as parameters
+         //   the select function returns data in a list
+        //    errors are displayed in message boxes
+
+        public static List<string> sqlSelect(string query) // perform select operation on database
         {
             MySqlConnection conn = new MySqlConnection(connStr);
             List<string> items = new List<string>();
@@ -26,7 +31,7 @@ namespace NEA_project
 
                 while (rdr.Read())
                 {
-                    items.Add(rdr.ToString());
+                    items.Add(rdr.ToString()); // read data and add each item to a string
                 }
                 rdr.Close();
                 
@@ -37,10 +42,10 @@ namespace NEA_project
             }
 
             conn.Close();
-            return items;
+            return items; // returns the string to where it was called from
         }
 
-        public static void sqlInsert(string query)
+        public static void sqlInsert(string query) // perform insert operation on database
         {
             MySqlConnection conn = new MySqlConnection(connStr);
             try
