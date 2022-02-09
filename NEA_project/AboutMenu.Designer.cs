d711f310f53f5caeb25428cc2965b97ae35503cb
will <williampaulgeorge@outlook.com>
2022-02-09 19:26:57 +0000

    start delete account feature

--- NEA_project/AboutMenu.Designer.cs
+++ NEA_project/AboutMenu.Designer.cs
@@ -32,10 +32,11 @@ namespace NEA_project
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutMenu));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.aboutBox = new System.Windows.Forms.GroupBox();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.idiotLabel = new System.Windows.Forms.Label();
             this.repoLabel = new System.Windows.Forms.LinkLabel();
             this.verLabel = new System.Windows.Forms.Label();
             this.nameLabel = new System.Windows.Forms.Label();
-            this.idiotLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.aboutBox.SuspendLayout();
             this.SuspendLayout();
@@ -51,16 +52,36 @@ namespace NEA_project
             // 
             // aboutBox
             // 
+            this.aboutBox.Controls.Add(this.deleteBtn);
             this.aboutBox.Controls.Add(this.idiotLabel);
             this.aboutBox.Controls.Add(this.repoLabel);
             this.aboutBox.Controls.Add(this.verLabel);
             this.aboutBox.Controls.Add(this.nameLabel);
             this.aboutBox.Location = new System.Drawing.Point(12, 51);
             this.aboutBox.Name = "aboutBox";
-            this.aboutBox.Size = new System.Drawing.Size(210, 98);
+            this.aboutBox.Size = new System.Drawing.Size(210, 117);
             this.aboutBox.TabIndex = 1;
             this.aboutBox.TabStop = false;
             this.aboutBox.Text = "About SMPP";
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(104, 88);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(100, 23);
+            this.deleteBtn.TabIndex = 4;
+            this.deleteBtn.Text = "Delete Account";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // idiotLabel
+            // 
+            this.idiotLabel.AutoSize = true;
+            this.idiotLabel.Location = new System.Drawing.Point(7, 53);
+            this.idiotLabel.Name = "idiotLabel";
+            this.idiotLabel.Size = new System.Drawing.Size(104, 15);
+            this.idiotLabel.TabIndex = 3;
+            this.idiotLabel.Text = "by William George";
             // 
             // repoLabel
             // 
@@ -91,20 +112,11 @@ namespace NEA_project
             this.nameLabel.TabIndex = 0;
             this.nameLabel.Text = "Stock Market Prediction Project";
             // 
-            // idiotLabel
-            // 
-            this.idiotLabel.AutoSize = true;
-            this.idiotLabel.Location = new System.Drawing.Point(7, 53);
-            this.idiotLabel.Name = "idiotLabel";
-            this.idiotLabel.Size = new System.Drawing.Size(104, 15);
-            this.idiotLabel.TabIndex = 3;
-            this.idiotLabel.Text = "by William George";
-            // 
             // AboutMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 161);
+            this.ClientSize = new System.Drawing.Size(234, 180);
             this.Controls.Add(this.aboutBox);
             this.Controls.Add(this.pictureBox1);
             this.Name = "AboutMenu";
@@ -124,5 +136,6 @@ namespace NEA_project
         private System.Windows.Forms.Label verLabel;
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Label idiotLabel;
+        private System.Windows.Forms.Button deleteBtn;
     }
 }
--- NEA_project/AboutMenu.cs
+++ NEA_project/AboutMenu.cs
@@ -26,5 +26,14 @@ namespace NEA_project
                 MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure? This will close the application", "Delete Account", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                // fetch user id and remove from db, needs delete statement in SQLOperations
+            }
+        }
     }
 }
