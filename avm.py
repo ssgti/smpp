d1e896ed310720db8aa9aa56c030577eccede582
will <williampaulgeorge@outlook.com>
2022-03-09 19:09:09 +0000

    exclude weekends

--- avm.py
+++ avm.py
@@ -8,45 +8,13 @@ from alpha_vantage.timeseries import TimeSeries
 mydb = mysql.connector.connect(host='127.0.0.1', user='snoop', password='snoopin321', database='mydb')
 mycursor = mydb.cursor()
 
-def getDates(): # date calculations for extracting relevant data
-	
-	# this code has multiple skill issues
-	# can't get data for weekends, markets closed
-	
-	date = datetime.datetime.now()
-	
-	year1 = date.year
-	month1 = date.strftime("%m")
-	
-	day1 = date.strftime("%d")
-	day1 = int(day1) - 1
-	d1 = (str(year1) + '-' + str(month1) + '-' + str(day1))
-	
-	day2 = date.strftime("%d")
-	day2 = int(day2) - 2
-	d2 = (str(year1) + '-' + str(month1) + '-' + str(day2))
-	
-	day3 = date.strftime("%d")
-	day3 = int(day3) - 3
-	d3 = (str(year1) + '-' + str(month1) + '-' + str(day3))
-	
-	day4 = date.strftime("%d")
-	day4 = int(day4) - 4
-	d4 = (str(year1) + '-' + str(month1) + '-' + str(day4))
-	
-	dates = (d1, d2, d3, d4)
-	
-	return dates
-	
-
 def getData(symbol): # retrieve and filter financial data
-	
 	# call API
 	ts = TimeSeries(key='LMLD4P0XA1H59J54')
 	data, meta_data = ts.get_daily(symbol=symbol, outputsize='compact')
 	
-	#filter data using date calculations
-	dates = getDates()
+	# filter data using date calculations
+	dates = setDateFormat()
 	close1 = data[dates[0]]
 	close2 = data[dates[1]]
 	close3 = data[dates[2]]
@@ -55,6 +23,30 @@ def getData(symbol): # retrieve and filter financial data
 	
 	return closes
 	
+def getMarketDays(s, end, excluded=(6, 7)): # get days when market is open (weekdays)
+    days = []
+    while s.date() <= end.date():
+        if s.isoweekday() not in excluded:
+            days.append(s)
+        s += datetime.timedelta(days = 1)
+    return days
+
+def setDateFormat(): # put dates into correct format for filtering API calls
+	today = datetime.datetime.now();
+	y = int(today.year)
+	m = int(today.month)
+	d = int(today.day)
+
+	days = getMarketDays(datetime.datetime(y, m, (d - 6)), datetime.datetime(y, m, d)) # get dates for relevant periods
+	dates = []
+	for x in range(len(days)):
+		year = days[x].year
+		month = days[x].strftime("%m")
+		day = days[x].strftime("%d")
+		dates.append(str(year) + '-' + str(month) + '-' + str(day)) # YYYY-MM-DD format
+	return dates
+
+
 
 # def updateDB(symbol):
 	
